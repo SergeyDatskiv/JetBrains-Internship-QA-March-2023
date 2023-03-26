0e4c93d9c6a62d494406e04c02022c107b459b14
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2023-03-27 00:32:04 +0200

    Created a validate() function for link component.

--- internship-qa-main/src/main/kotlin/org/example/internship/data/Components.kt
+++ internship-qa-main/src/main/kotlin/org/example/internship/data/Components.kt
@@ -137,6 +137,51 @@ class Format : Component(), InlineComponent {
 @SerialName("link")
 class Link : Component(), InlineComponent {
     var href: String? by Properties()
+    var allowedProtocols: MutableSet<String> = mutableSetOf<String>("http", "https")
+
+    /**
+     * Validate is link specific function which checks the validity of a link.
+     */
+    override fun validate(): MutableList<ValidityReport> {
+        var message: String = ""
+        var reports: MutableList<ValidityReport> = mutableListOf()
+        var checkProtocol: Boolean = false
+        if (href.isNullOrBlank()) {
+            isValid = false
+            message += "There is no href."
+        } else {
+            for (protocol in allowedProtocols) {
+                // TODO: Check if URL is valid.
+                checkProtocol = href!!.contains(protocol)
+                if (checkProtocol) {
+                    break
+                }
+                isValid = false
+                message += "The protocol of a link is not http or https. ($href) "
+            }
+        }
+        if (children.size >= 1) {
+            for (child in children) {
+                if (child !is Text) {
+                    isValid = false
+                    message += "A child ($child) is not a Text component. Only Text components are allowed.\n"
+                }
+                var childValidityReport: MutableList<ValidityReport> = child.validate()
+                reports.addAll(childValidityReport)
+                for (childReport in childValidityReport) {
+                    if (!childReport.isValid) {
+                        isValid = false
+                        message += "This child component (${childReport.component}) is invalid.\n"
+                    }
+                }
+            }
+        } else {
+            isValid = false
+            message += "The link is empty. "
+        }
+        reports.add(ValidityReport(this, message, isValid))
+        return reports
+    }
 }
 
 @Serializable
