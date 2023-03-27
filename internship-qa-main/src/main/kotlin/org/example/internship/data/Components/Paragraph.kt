a0cb3ea26185bbc1a661c513c12dd46305486209
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2023-03-27 21:58:44 +0200

    Refactored Paragraph.kt.

--- internship-qa-main/src/main/kotlin/org/example/internship/data/Components/Paragraph.kt
+++ internship-qa-main/src/main/kotlin/org/example/internship/data/Components/Paragraph.kt
@@ -4,35 +4,70 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.example.internship.data.ValidityReport
 
+// Message constants.
+private const val AT_LEAST_ONE_CHILD = 1
+private const val EMPTY_PARAGRAPH = "The paragraph is empty. "
+
 @Serializable
 @SerialName("paragraph")
 class Paragraph : Component(), BlockComponent {
+
     /**
-     * Validate is Paragraph specific function which checks the validity of a Paragraph.
+     * [validate] is Paragraph specific function which checks the validity of a Paragraph.
+     * [checkChildren] check requirements related to the children of a Paragraph.
+     * Returns a MutableList of [ValidityReport].
      */
     override fun validate(): MutableList<ValidityReport> {
-        var message: String = ""
-        var reports: MutableList<ValidityReport> = mutableListOf()
-        if (children.size >= 1) {
-            for (child in children) {
-                if (child !is InlineComponent) {
-                    isValid = false
-                    message += "A child ($child) is not an inline component. Only inline components are allowed.\n"
-                }
-                var childValidityReport: MutableList<ValidityReport> = child.validate()
-                reports.addAll(childValidityReport)
-                for (childReport in childValidityReport) {
-                    if (!childReport.isValid) {
-                        isValid = false
-                        message += "This child component (${childReport.component}) is invalid.\n"
-                    }
-                }
-            }
-        } else {
-            isValid = false
-            message += "The paragraph is empty. "
-        }
+        var message = ""
+        val reports: MutableList<ValidityReport> = mutableListOf()
+        message = checkChildren(message, reports)
         reports.add(ValidityReport(this, message, isValid))
         return reports
+    }
+
+    /**
+     * [checkChildren] checks requirements related to the children of a Paragraph.
+     * [AT_LEAST_ONE_CHILD] checks if a Paragraph has at least one child.
+     * [checkEachChild] checks validity of individual child component.
+     * Returns an informative message.
+     */
+    private fun checkChildren(message: String, reports: MutableList<ValidityReport>): String {
+        var outputMessage = message
+        outputMessage = if (children.size >= AT_LEAST_ONE_CHILD) {
+            checkEachChild(outputMessage, reports)
+        } else {
+            invalidate(outputMessage, EMPTY_PARAGRAPH)
+        }
+        return outputMessage
+    }
+
+    /**
+     * [checkEachChild] checks validity of individual child component.
+     * [checkChildIsInlineComponent] checks if a child is of [InlineComponent] type.
+     * [generateChildValidityReports] generates a [ValidityReport] for each child.
+     * Returns an informative message.
+     */
+    private fun checkEachChild(message: String, reports: MutableList<ValidityReport>): String {
+        var outputMessage = message
+        for (child in children) {
+            outputMessage = checkChildIsInlineComponent(child, outputMessage)
+            outputMessage = generateChildValidityReports(child, reports, outputMessage)
+        }
+        return outputMessage
+    }
+
+    /**
+     * [checkChildIsInlineComponent] checks if a child is of [InlineComponent] type.
+     * Returns an informative message.
+     */
+    private fun checkChildIsInlineComponent(child: Component, message: String): String {
+        var outputMessage = message
+        if (child !is InlineComponent) {
+            outputMessage = invalidate(
+                outputMessage,
+                "A child ($child) is not an inline component. Only inline components are allowed.\n"
+            )
+        }
+        return outputMessage
     }
 }
