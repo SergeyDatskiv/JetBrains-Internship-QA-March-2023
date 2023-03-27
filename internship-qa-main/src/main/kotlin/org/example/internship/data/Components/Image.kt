3d7d056d32f242ea3b5d229928d55cb68224aa07
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2023-03-27 20:47:40 +0200

    Refactored Image.kt.

--- internship-qa-main/src/main/kotlin/org/example/internship/data/Components/Image.kt
+++ internship-qa-main/src/main/kotlin/org/example/internship/data/Components/Image.kt
@@ -3,45 +3,94 @@ package org.example.internship.data.Components
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.example.internship.data.ValidityReport
+import kotlin.collections.List
+
+// Message constants.
+private const val CHILDREN_PRESENT = "An image should not have children. "
+private const val PNG = "png"
+private const val JPG = "jpg"
+private const val JPEG = "jpeg"
+private const val SVG = "svg"
 
 @Serializable
 @SerialName("image")
 class Image : Component(), BlockComponent {
     var src: String? by Properties()
     var darkSrc: String? by Properties()
-    var validImageFormats: Set<String> = setOf("png", "jpg", "jpeg", "svg")
+    private var validImageFormats: Set<String> = setOf(PNG, JPG, JPEG, SVG)
 
     /**
-     * Validate is Image specific function which checks the validity of an Image.
+     * [validate] is Image specific function which checks the validity of an Image.
+     * [checkSrcImage] checks the source image requirements of an Image.
+     * [checkChildren] check requirements related to the children of an Image.
+     * [checkDarkSrcImage] checks the dark source image requirements of an Image.
+     * Returns a MutableList of [ValidityReport].
      */
     override fun validate(): MutableList<ValidityReport> {
-        var message: String = ""
-        val srcCheck: kotlin.collections.List<String>? = src?.split(".")
-        val darkSrcCheck: kotlin.collections.List<String>? = darkSrc?.split(".")
+        var message = ""
+        val srcCheck: List<String>? = src?.split(".")
+        val darkSrcCheck: List<String>? = darkSrc?.split(".")
+        message = checkSrcImage(srcCheck, message)
+        message = checkChildren(message)
+        message = checkDarkSrcImage(darkSrcCheck, message)
+        return mutableListOf(ValidityReport(this, message, isValid))
+    }
+
+    /**
+     * [invalidate] is a function that sets the [isValid] variable to false.
+     * It also appends a specified [errorMessage] to the [inputMessage].
+     * Returns an informative message.
+     */
+    private fun invalidate(inputMessage: String, errorMessage: String): String {
+        isValid = false
+        return inputMessage + errorMessage
+    }
+
+    /**
+     * [checkSrcImage] checks if the dark source image adheres to the requirements.
+     * The image format is supported by [validImageFormats].
+     * Checks that a source is specified for an Image.
+     * Also checks if a name of an image does not have ".".
+     */
+    private fun checkSrcImage(srcCheck: List<String>?, message: String): String {
+        var outputMessage = message
         if (srcCheck != null && srcCheck.size == 2) {
             if (!validImageFormats.contains(srcCheck[1])) {
-                isValid = false
-                message += "Unsupported (${srcCheck[1]}) image format. "
+                outputMessage = invalidate(outputMessage, "Unsupported (${srcCheck[1]}) image format. ")
             }
         } else {
-            isValid = false
-            message += "Missing or invalid name of an image ($src). "
+            outputMessage = invalidate(outputMessage, "Missing or invalid name of an image ($src). ")
         }
+        return outputMessage
+    }
+
+    /**
+     * [checkChildren] checks requirements related to the children of an Image.
+     * [AT_LEAST_ONE_CHILD] checks if an Image has no children.
+     * Returns an informative message.
+     */
+    private fun checkChildren(message: String): String {
+        var outputMessage = message
         if (children.isNotEmpty()) {
-            isValid = false
-            message += "An image should not have children. "
+            outputMessage = invalidate(outputMessage, CHILDREN_PRESENT)
         }
+        return outputMessage
+    }
+
+    /**
+     * [checkDarkSrcImage] checks if the dark source image adheres to the requirements.
+     * The image format is supported by [validImageFormats].
+     * Also checks if a name of an image does not have ".".
+     */
+    private fun checkDarkSrcImage(darkSrcCheck: List<String>?, message: String): String {
+        var outputMessage = message
         if (darkSrcCheck != null && darkSrcCheck.size == 2) {
             if (!validImageFormats.contains(darkSrcCheck[1])) {
-                isValid = false
-                message += "Dark version has unsupported (${darkSrcCheck[1]}) image format. "
+                outputMessage = invalidate(outputMessage, "Dark version has unsupported (${darkSrcCheck[1]}) image format. ")
             }
-        } else if (darkSrcCheck != null) {
-            if (darkSrcCheck.size != 2) {
-                isValid = false
-                message += "invalid name of an image ($darkSrc). "
-            }
+        } else if (darkSrcCheck != null && darkSrcCheck.size != 2) {
+            outputMessage = invalidate(outputMessage, "invalid name of an image ($darkSrc). ")
         }
-        return mutableListOf<ValidityReport>(ValidityReport(this, message, isValid))
+        return outputMessage
     }
 }
