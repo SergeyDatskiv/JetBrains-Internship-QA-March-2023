5731c8a505767dfbae0b24c072c90167fd469177
Sergey Datskiv <S.Datskiv@student.tudelft.nl>
2023-03-27 21:45:43 +0200

    Refactored List.kt.

--- internship-qa-main/src/main/kotlin/org/example/internship/data/Components/List.kt
+++ internship-qa-main/src/main/kotlin/org/example/internship/data/Components/List.kt
@@ -4,62 +4,139 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.example.internship.data.ValidityReport
 
+// Message constants.
+private const val BULLET = "bullet"
+private const val NUMERICAL = "numerical"
+private const val PLAIN = "plain"
+private const val CIRCLE = "circle"
+private const val SQUARE = "square"
+private const val MISSING_LIST_STYLE = "Missing list style. "
+private const val EMPTY_LIST = "A list does not have children (i.e. empty list). "
+private const val AT_LEAST_ONE_CHILD = 1
+
 @Serializable
 @SerialName("list")
 class List : Component(), BlockComponent {
     var style: String? by Properties()
     var startWith: String? by Properties()
     var bulletType: String? by Properties()
-    var allowedStyles: MutableSet<String> = mutableSetOf("bullet", "numerical", "plain")
-    var allowedBulletType: MutableSet<String> = mutableSetOf("circle", "square")
+    private var allowedStyles: MutableSet<String> = mutableSetOf(BULLET, NUMERICAL, PLAIN)
+    private var allowedBulletType: MutableSet<String> = mutableSetOf(CIRCLE, SQUARE)
 
     /**
-     * Validate is list specific function which checks the validity of a list.
+     * [validate] is List specific function which checks the validity of a List.
+     * [checkStyle] checks style requirements of a List.
+     * [checkNumericalStyle] checks numerical style requirements of a List.
+     * [checkBulletStyle] checks bullet style requirements of a List.
+     * [checkStyleIsAllowed] checks if a list is of allowed type.
+     * [checkChildren] check requirements related to the children of a List.
+     * Returns a MutableList of [ValidityReport].
      */
     override fun validate(): MutableList<ValidityReport> {
-        var message: String = ""
-        var reports: MutableList<ValidityReport> = mutableListOf()
-        if (style.isNullOrBlank()) {
-            isValid = false
-            message += "Missing list style. "
-        }
-        if (style.equals("numerical")) {
-            var startInt: Int? = startWith?.toInt()
-            if (startInt != null && startInt < 1) {
-                isValid = false
-                message += "Starting integer ($startWith) is invalid. "
-            }
-        }
-        if (style.equals("bullet")) {
-            if (bulletType != null && !allowedBulletType.contains(bulletType)) {
-                isValid = false
-                message += "This bullet type ($bulletType) is invalid."
-            }
-        }
-        if (!allowedStyles.contains(style)) {
-            isValid = false
-            message += "This style ($style) is not supported for the list. "
-        }
-        if (children.size < 1) {
-            isValid = false
-            message += "A list does not have children (i.e. empty list). "
-        } else {
-            for (child in children) {
-                if (child !is BlockComponent) {
-                    isValid = false
-                    message += "A child ($child) is not a block component. Only block components are allowed.\n"
-                }
-                var childValidityReport: MutableList<ValidityReport> = child.validate()
-                reports.addAll(childValidityReport)
-                for (childReport in childValidityReport) {
-                    if (!childReport.isValid) {
-                        isValid = false
-                        message += "This child component (${childReport.component}) is invalid.\n"
-                    }
-                }
-            }
-        }
+        var message = ""
+        val reports: MutableList<ValidityReport> = mutableListOf()
+        message = checkStyle(message)
+        message = checkNumericalStyle(message)
+        message = checkBulletStyle(message)
+        message = checkStyleIsAllowed(message)
+        message = checkChildren(message, reports)
         reports.add(ValidityReport(this, message, isValid))
         return reports
+    }
+
+    /**
+     * [checkStyle] checks style requirements of a List.
+     * Returns an informative message.
+     */
+    private fun checkStyle(message: String): String {
+        var outputMessage = message
+        if (style.isNullOrBlank()) {
+            outputMessage = invalidate(outputMessage, MISSING_LIST_STYLE)
+        }
+        return outputMessage
+    }
+
+    /**
+     * [checkNumericalStyle] checks numerical style requirements of a List.
+     * Returns an informative message.
+     */
+    private fun checkNumericalStyle(message: String): String {
+        var outputMessage = message
+        if (style.equals(NUMERICAL)) {
+            val startInt: Int? = startWith?.toInt()
+            if (startInt != null && startInt < AT_LEAST_ONE_CHILD) {
+                outputMessage = invalidate(outputMessage, "Starting integer ($startWith) is invalid. ")
+            }
+        }
+        return outputMessage
+    }
+
+    /**
+     * [checkBulletStyle] checks bullet style requirements of a List.
+     * Returns an informative message.
+     */
+    private fun checkBulletStyle(message: String): String {
+        var outputMessage = message
+        if (style.equals(BULLET)) {
+            if (bulletType != null && !allowedBulletType.contains(bulletType)) {
+                outputMessage = invalidate(outputMessage, "This bullet type ($bulletType) is invalid.")
+            }
+        }
+        return outputMessage
+    }
+
+    /**
+     * [checkStyleIsAllowed] checks if a list is of allowed type.
+     * Returns an informative message.
+     */
+    private fun checkStyleIsAllowed(message: String): String {
+        var outputMessage = message
+        if (!allowedStyles.contains(style)) {
+            outputMessage = invalidate(outputMessage, "This style ($style) is not supported for the list. ")
+        }
+        return outputMessage
+    }
+
+    /**
+     * [checkChildren] checks requirements related to the children of a Format.
+     * [AT_LEAST_ONE_CHILD] checks if a Format has at least one child.
+     * [checkEachChild] checks validity of individual child component.
+     * Returns an informative message.
+     */
+    private fun checkChildren(message: String, reports: MutableList<ValidityReport>): String {
+        var outputMessage = message
+        outputMessage = if (children.size < AT_LEAST_ONE_CHILD) {
+            invalidate(outputMessage, EMPTY_LIST)
+        } else {
+            checkEachChild(outputMessage, reports)
+        }
+        return outputMessage
+    }
+
+    /**
+     * [checkEachChild] checks validity of individual child component.
+     * [checkChildIsBlockComponent] checks if a child is of [BlockComponent] type.
+     * [generateChildValidityReports] generates a [ValidityReport] for each child.
+     * Returns an informative message.
+     */
+    private fun checkEachChild(message: String, reports: MutableList<ValidityReport>): String {
+        var outputMessage = message
+        for (child in children) {
+            outputMessage = checkChildIsBlockComponent(child, outputMessage)
+            outputMessage = generateChildValidityReports(child, reports, outputMessage)
+        }
+        return outputMessage
+    }
+
+    /**
+     * [checkChildIsBlockComponent] checks if a child is of [BlockComponent] type.
+     * Returns an informative message.
+     */
+    private fun checkChildIsBlockComponent(child: Component, message: String): String {
+        var outputMessage = message
+        if (child !is BlockComponent) {
+            outputMessage = invalidate(outputMessage, "A child ($child) is not a block component. Only block components are allowed.\n")
+        }
+        return outputMessage
     }
 }
